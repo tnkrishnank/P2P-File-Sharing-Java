6ad8c8874cb814a23a57268612e3268295a3b5be
Navin Krishna <tnkrishnank@gmail.com>
2022-07-01 11:52:03 +0530

    Initial commit

---
+++ Peer.java
@@ -0,0 +1,11 @@
+import java.io.*;
+
+public class Peer {
+
+    public static void main(String[] args) throws IOException, InterruptedException {
+
+        Menu menu = new Menu();
+        menu.start();
+        menu.join();
+    }
+}
